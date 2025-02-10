6737a6e54890edd504a28332eca5ed1f827b7d61
germaine <germaine.goetzelmann@kit.edu>
2025-02-10 12:06:57 +0100

    test for workaround fix of pretilt

---
+++ tests/model_tests/test_dataset.py
@@ -0,0 +1,20 @@
+import unittest
+
+from src.model.SchemaConcepts.Dataset_simplified import Dataset
+
+class TestDataset(unittest.TestCase):
+
+    def test_pretilt(self):
+        """
+        This specific test is there to detect problems with unwanted definition in the current schema and a bug in pydantic.
+        1. instrument.eBeam.preTilt has an erronous space in the schema definition. In the strictest sense the output of the mapper is not schema conform here
+        2. pydantic 2.10 has trouble honoring nested aliases on model_dump. This should be addressed in 2.11 - workaround fix could then be removed
+        :return:
+        """
+        instrument_md = {
+            "eBeam": {
+                "preTilt": {"value": 1234}
+            }
+        }
+
+        ds = Dataset(instrument=instrument_md)
