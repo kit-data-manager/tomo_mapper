7bc7403351df2ce7bb9b1a134eaeaeac325e046b
Germaine Götzelmann <germaine.goetzelmann@kit.edu>
2025-02-27 18:21:31 +0100

    CSV mapping replaced by json for SetupMD Parsers

--- src/resources/maps/mapping/__init__.py
+++ src/resources/maps/mapping/__init__.py
@@ -1,8 +1,12 @@
 from importlib import resources
 
 files = resources.files(__name__)
+
 tiffparser_sem_34682 = files.joinpath("SEM_thermofisher.json")
 tiffparser_tomo_34682 = files.joinpath("TOMO_thermofisher_image.json")
 tiffparser_sem_34118 = files.joinpath("SEM_zeiss.json")
 tiffparser_tomo_51023 = files.joinpath("TOMO_zeiss_image.json")
+
+setup_zeiss = files.joinpath("TOMO_zeiss_setup.json")
+setup_tf = files.joinpath("TOMO_thermofisher_setup.json")
 
